3d0e89e7e8908d93375e18958c2fe6f1703e30a3
Talha Anjum <chtalha.anjum@gmail.com>
2023-10-30 17:20:54 +0500

    .yml and index.js changes

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,6 +1,23 @@
-const codeToSend = process.env.CODE_TO_SEND;
+const fs = require("fs");
 
-console.log("Received code:");
-console.log(codeToSend);
+// Define the file path you want to read
+const filePath = process.argv[2]; // Assumes the file path is passed as an argument
 
-// Further processing of codeToSend
+// Check if a file path was provided
+if (!filePath) {
+  console.error("Usage: node index.js <file_path>");
+  process.exit(1);
+}
+
+// Read the file
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error(`Error reading the file: ${err.message}`);
+  } else {
+    // Process the file content (data)
+    console.log("File content:");
+    console.log(data);
+
+    // You can perform additional processing on the file content here
+  }
+});
