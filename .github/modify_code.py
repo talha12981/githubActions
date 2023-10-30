24fbd2ffd16b7fc10e801f1b814a693512dcb3b0
Talha Anjum <chtalha.anjum@gmail.com>
2023-10-30 16:03:18 +0500

    Added the actions and modify code files

---
+++ .github/modify_code.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+def replace_function_name(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+        print("Content: ", content);
+
+# Get the file path from the command line arguments
+if len(sys.argv) != 2:
+    print("Usage: python modify_code.py <file_path>")
+    sys.exit(1)
+
+file_path = sys.argv[1]
+
+replace_function_name(file_path)
